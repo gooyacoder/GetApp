662fd2f29ef7b329136d23f023c9c2025de3ee3e
amirhosein <gooyacoder@gmail.com>
2024-02-06 21:59:28 +0330

    17_11_1402  21:59

--- app/src/main/java/com/ahm/getapp/MainActivity.kt
+++ app/src/main/java/com/ahm/getapp/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.ahm.getapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_CODE_PERMISSIONS = 2
+
+    private val REQUIRED_PERMISSIONS = arrayOf<String>(
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        if (allPermissionsGranted()) {
+            // Permissions granted, proceed with the app
+            // startApp();
+        } else {
+            // Request permissions
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
+        }
+    }
+    private fun allPermissionsGranted(): Boolean {
+        for (permission in REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    permission
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return false
+            }
+        }
+        return true
     }
 }
