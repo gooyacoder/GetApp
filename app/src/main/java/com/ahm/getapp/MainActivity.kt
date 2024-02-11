2b621ced476ecbd46d9b3d96f9c86741a6b01044
amirhosein <gooyacoder@gmail.com>
2024-02-11 20:03:40 +0330

    22_11_1402  16:35

--- app/src/main/java/com/ahm/getapp/MainActivity.kt
+++ app/src/main/java/com/ahm/getapp/MainActivity.kt
@@ -3,9 +3,13 @@ package com.ahm.getapp
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Environment
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import java.io.File
+import java.io.FileOutputStream
 
 
 class MainActivity : AppCompatActivity() {
@@ -40,5 +44,21 @@ class MainActivity : AppCompatActivity() {
             }
         }
         return true
+    }
+
+    private fun download(url: String){
+        var fileName = "TestFile"
+        val file = File(
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
+            fileName
+        )
+        val output = FileOutputStream(file)
+        output.write("Test Text".toByteArray())
+        output.flush()
+        output.close()
+    }
+
+    fun onDownloadButtonClicked(view: View) {
+        download("")
     }
 }
